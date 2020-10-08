13efd84cfdb5d0f3294aa9c6eb1dabe01d861977
cristina.i <cristina.i@adoreme.com>
2020-10-08 22:10:22 +0300

    exercitiile de la M01 S02 si S03

---
+++ M01S03/ex01/app.js
@@ -0,0 +1,14 @@
+console.log(object.age);
+console.log(object.surname);
+console.log(
+  'Ma numesc ' +
+    object.name +
+    ' ' +
+    object.surname +
+    ' si sunt ' +
+    object.age +
+    'ish :)',
+);
+
+// Afiseaza anul
+console.log(2020 - object.age / 2);
